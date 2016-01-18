e7ba60d1dc799935c762a6304b9c8bf48d070f33
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-01-18 12:44:52 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='oidc-fed',
+    version='0.0.1',
+    description='Example implementation of model for OpenID Connect federations.',
+    author='DIRG',
+    author_email='dirg@its.umu.se',
+    license='Apache 2.0',
+    url='https://github.com/its-dirg/oidc-fed',
+    packages=find_packages('src/'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "oic",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
---
+++ src/oidc_fed/__init__.py
@@ -0,0 +1,7 @@
+"""
+    oidc-fed
+    ~~~~~~~~~~~~~~~~
+    Example implementation of model for OpenID Connect federations.
+    :copyright: (c) 2016 by Umeå University.
+    :license: APACHE 2.0, see LICENSE for more details.
+"""
