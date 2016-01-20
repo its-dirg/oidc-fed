1f0f1df3c4acdee81eda3b8a210a039aa9da959f
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-01-20 10:18:03 +0100

    Add OP class capable of generating provider configuration information.

---
+++ src/oidc_fed/provider.py
@@ -0,0 +1,37 @@
+from oic.oic.provider import Provider
+
+from oidc_fed import OIDCFederationEntity
+from oidc_fed.messages import FederationProviderConfigurationResponse
+
+
+class OP(OIDCFederationEntity):
+    def __init__(self, issuer, root_key, software_statements, federation_keys, signed_jwks_uri,
+                 jwks_uri=None):
+        # type: (str, Key, Sequence[str], Sequence[Key], str, Optional[str]) -> None
+        """
+        :param issuer: issuer URL for this provider
+        :param root_key: root signing key for this OP
+        :param software_statements: all software statements isssued by federations for this OP
+        :param federation_keys: public keys from all federations this OP is part of
+        :param signed_jwks_uri: URL endpoint where the signed JWKS is published
+        :param jwks_uri: URL endpoint where the JWKS is published
+        """
+        super(OP, self).__init__(root_key, software_statements, federation_keys)
+        self.signed_jwks_uri = signed_jwks_uri
+        self.provider = Provider(issuer, None, None, None, None, None, None, None)
+        self.provider.jwks_uri = jwks_uri
+
+    def provider_configuration(self):
+        # type: () -> FederationProviderConfigurationResponse
+        """
+        Generate the provider configuration information.
+        :return: the provider configuration information
+        """
+        extra_params = dict(software_statements=self.software_statements,
+                            signing_key=self.signed_intermediary_key,
+                            signed_jwks_uri=self.signed_jwks_uri)
+        provider_config = self.provider.create_providerinfo(
+                pcr_class=FederationProviderConfigurationResponse, setup=extra_params)
+        provider_config["signed_metadata"] = self._sign(provider_config.to_dict(), self.intermediary_key)
+
+        return provider_config
---
+++ tests/oidc_fed/test_provider.py
@@ -0,0 +1,42 @@
+import json
+
+from Crypto.PublicKey import RSA
+from jwkest.jwk import RSAKey, SYMKey
+from jwkest.jws import JWS
+from oic.oauth2 import rndstr
+
+from oidc_fed.federation import Federation
+from oidc_fed.provider import OP
+
+
+def rsa_key():
+    return RSAKey(key=RSA.generate(1024), use="sig", alg="RS256", kid=rndstr(4))
+
+
+def sym_key():
+    return SYMKey(k=rndstr(), use="sig", alg="HS256", kid=rndstr(4))
+
+
+class TestOP(object):
+    def test_provider_configuration(self):
+        issuer = "https://op.example.com"
+        signed_jwks_uri = "{}/signed_jwks".format(issuer)
+        federation_key = sym_key()
+        op_root_key = rsa_key()
+        op_registration_data = dict(root_key=json.dumps(op_root_key.serialize(private=False)))
+        op_software_statement = Federation(federation_key).create_software_statement(
+                op_registration_data)
+
+        op = OP(issuer, op_root_key, [op_software_statement], None, signed_jwks_uri)
+
+        provider_config = op.provider_configuration()
+        assert provider_config["issuer"] == issuer
+        assert provider_config["software_statements"] == [op_software_statement]
+        assert provider_config["signing_key"] == op.signed_intermediary_key
+        assert provider_config["signed_jwks_uri"] == signed_jwks_uri
+
+        expected_metadata_parameters = set(provider_config.keys())
+        expected_metadata_parameters.remove("signed_metadata")
+        actual_metadata_parameters = JWS().verify_compact(provider_config["signed_metadata"],
+                                                          keys=[op.intermediary_key]).keys()
+        assert set(actual_metadata_parameters) == expected_metadata_parameters
