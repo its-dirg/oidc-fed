988cfe73a9fe00f25dca60c7e4a393e98d4564f1
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-01-26 16:28:34 +0100

    Verify registration request.
    Matches provider capabilities (from config and provider software
    statement) with client metadata (from registration request and client
    software statement).

--- src/oidc_fed/provider.py
+++ src/oidc_fed/provider.py
@@ -1,12 +1,21 @@
-from oic.oic.provider import Provider
+import copy
+import json
 
-from oidc_fed import OIDCFederationEntity
-from oidc_fed.messages import FederationProviderConfigurationResponse
+from oic.exception import MessageException
+from oic.extension.signed_http_req import SignedHttpRequest, ValidationError
+from oic.oic import PREFERENCE2PROVIDER
+from oic.oic.provider import Provider
+from oic.utils.http_util import Response
+
+from oidc_fed import OIDCFederationEntity, OIDCFederationError
+from oidc_fed.messages import FederationProviderConfigurationResponse, \
+    FederationRegistrationRequest, \
+    FederationRegistrationResponse
 
 
 class OP(OIDCFederationEntity):
     def __init__(self, issuer, root_key, software_statements, federation_keys, signed_jwks_uri,
-                 jwks_uri=None):
+                 jwks_uri=None, capabilities=None):
         # type: (str, Key, Sequence[str], Sequence[Key], str, Optional[str]) -> None
         """
         :param issuer: issuer URL for this provider
@@ -17,8 +26,11 @@ class OP(OIDCFederationEntity):
         :param jwks_uri: URL endpoint where the JWKS is published
         """
         super(OP, self).__init__(root_key, software_statements, federation_keys, signed_jwks_uri)
-        self.provider = Provider(issuer, None, None, None, None, None, None, None)
+        self.provider = Provider(issuer, None, {}, None, None, None, None, None,
+                                 capabilities=capabilities)
         self.provider.jwks_uri = jwks_uri
+
+        self.registration_verification = RegistrationRequestVerification()
 
     def provider_configuration(self):
         # type: () -> FederationProviderConfigurationResponse
@@ -32,6 +44,188 @@ class OP(OIDCFederationEntity):
                             signed_jwks_uri=self.signed_jwks_uri)
         provider_config = self.provider.create_providerinfo(
                 pcr_class=FederationProviderConfigurationResponse, setup=extra_params)
-        provider_config["signed_metadata"] = self._sign(provider_config.to_dict(), self.intermediary_key)
+        provider_config["signed_metadata"] = self._sign(provider_config.to_dict(),
+                                                        self.intermediary_key)
 
         return provider_config
+
+    def register_client(self, request_headers, request_body):
+        # type (Mapping[str, str], str) -> Response
+        """
+        Process a signed registration request from a client.
+        :param request_headers: HTTP request headers
+        :param request_body: unpacked HTTP POST body
+        :raise OIDCFederationError: if the registration request is malformed
+        :return: the registered client metadata to return to the client
+        """
+        if "Authorization" not in request_headers:
+            raise OIDCFederationError("Missing Authorization header in registration request.")
+        if not request_headers["Authorization"].startswith("pop "):
+            raise OIDCFederationError("Wrong Authentication scheme in registration request.")
+
+        registration_request = FederationRegistrationRequest(**json.loads(request_body))
+        registration_request.rm_blanks()
+
+        try:
+            registration_request.verify()
+        except MessageException as e:
+            raise OIDCFederationError("Error in client registration request: {}.".format(str(e)))
+
+        client_software_statement, client_signing_key = self._verify_signature_chain(
+                registration_request["software_statements"],
+                registration_request["signing_key"])
+
+        request_signature = request_headers["Authorization"][len("pop "):]
+        try:
+            SignedHttpRequest(client_signing_key).verify(request_signature, body=request_body)
+        except ValidationError as e:
+            raise OIDCFederationError("Could not verify signature of client registration request.")
+
+        provider_software_statement = self._find_software_statement_for_federation(
+                client_software_statement.jwt.headers["kid"])
+        matched_preferences = self.registration_verification.verify(
+                self.provider.capabilities.to_dict(), provider_software_statement.msg,
+                registration_request.to_dict(), client_software_statement.msg)
+
+        # recreate client registration request only with the common capabilities
+        registration_request.update(matched_preferences)
+        result = self.provider.client_registration_setup(registration_request)
+        if isinstance(result, Response):
+            return result
+
+        registration_response = FederationRegistrationResponse(**result.to_dict())
+        registration_response["provider_software_statement"] = self._recreate_software_statement(
+                provider_software_statement)
+        return registration_response
+
+    def _find_software_statement_for_federation(self, federation_kid):
+        # type: (str) -> Optional[JWS]
+        """
+        Find a software statement signed by the specified key id.
+        :param federation_kid: key id to search for
+        :return: the first occurrence of a software statement signed by the specified key id
+        """
+        for ss in self.software_statements:
+            if ss.jwt.headers["kid"] == federation_kid:
+                return ss
+
+        return None
+
+
+class RegistrationRequestVerification(object):
+    """Client metadata attributes which are JSON arrays in registration request."""
+    METADATA_AS_ARRAYS = [("response_types", "response_types_supported"),
+                          ("grant_types", "grant_types_supported"),
+                          ("default_acr_values", "acr_values_supported")]
+
+    def verify(self, provider_capabilities, provider_software_statement, client_preferences,
+               client_software_statement):
+        # type: (Mapping[str, Union[str, Sequence[str]]],
+        #        Mapping[str, Union[str, Sequence[str]]],
+        #        Mapping[str, Union[str, Sequence[str]]],
+        #        Mapping[str, Union[str, Sequence[str]]]) -> Dict[str, Union[str, List[str]]]
+        """
+        Verify the provider can satisfy the client's registration request.
+
+
+        Negotiates the metadata parameters combined from the provider metadata, provider software
+        statement, client metadata, and client software statement.
+
+        :param provider_capabilities: provider metadata
+        :param provider_software_statement: provider software statement
+        :param client_preferences: client metadata
+        :param client_software_statement: client software statement
+        :raise OIDCFederationError: if the client metadata can't be matched with the provider's
+                                    capabilities
+        :return: the "negotiated" client metadata parameters that overrides/extends the initial
+                 registration request
+        """
+        client_preferences = copy.deepcopy(client_preferences)
+        client_preferences.update(client_software_statement)
+        provider_capabilities = copy.deepcopy(provider_capabilities)
+        provider_capabilities.update(provider_software_statement)
+
+        client_preferences.update(
+                self._match_array_preferences(client_preferences, provider_capabilities))
+
+        for client_preference, provider_capability in [v for v in
+                                                       PREFERENCE2PROVIDER.items() if
+                                                       v not in RegistrationRequestVerification.METADATA_AS_ARRAYS]:
+            if client_preference not in client_preferences and provider_capability in provider_software_statement:
+                # default to metadata from provider software statement to ensure restrictions from
+                # the federation are applied
+                restrictions = provider_software_statement[provider_capability]
+                # just select the first, but could be some smarter heuristic
+                client_preferences[client_preference] = restrictions[0]
+                continue
+
+            try:
+                client_value = client_preferences[client_preference]
+                provider_values = provider_capabilities[provider_capability]
+            except KeyError:
+                continue
+
+            if client_value not in provider_values:
+                raise OIDCFederationError(
+                        "Mismatch in registration request: {} '{}' not in {} '{}'.".format(
+                                client_preference, client_value, provider_capability,
+                                provider_values))
+
+        return client_preferences
+
+    def _match_array_preferences(self, client_preferences, provider_capabilities):
+        # type: (Mapping[str, Union[str, Sequence[str]]],
+        #        Mapping[str, Union[str, Sequence[str]]]) -> Dict[str, Union[str, List[str]]]
+        """
+
+        :param client_preferences: client metadata
+        :param provider_capabilities: provider metadata
+        :raise OIDCFederationError: if no common metadata parameters can be found
+        :return: the negotiated/matching metadata
+        """
+        matching_preferences = {}
+        if "response_types" in client_preferences:
+            matching_preferences["response_types"] = self._match_response_types(
+                    client_preferences["response_types"],
+                    provider_capabilities["response_types_supported"])
+
+        for client_preference, provider_capability in RegistrationRequestVerification.METADATA_AS_ARRAYS:
+            if client_preference == "response_types":
+                # skip as it's already been handled
+                continue
+
+            if client_preference in client_preferences:
+                client_values = set(client_preferences[client_preference])
+                provider_values = set(provider_capabilities[provider_capability])
+                common_values = client_values & provider_values
+                if not common_values:
+                    raise OIDCFederationError(
+                            "Mismatch in registration request, no common {} from client '{}' and provider {} '{}'.".format(
+                                    client_preference, client_values, provider_capability,
+                                    provider_values))
+
+                matching_preferences[client_preference] = list(common_values)
+
+        return matching_preferences
+
+    def _match_response_types(self, client_response_types, provider_response_types):
+        # type: (Sequence[str], Sequence[str]) -> List[str]
+        """
+
+        :param client_response_types: response types the client wants to register, where each
+                                      response type is space-separated,
+        :param provider_response_types: response types supported by the provider, where each
+                                        response type is space-separated
+        :return: the matching response types as space-separated strings
+        """
+        client_values = {frozenset(response_type.split(" ")) for response_type in
+                         client_response_types}
+        provider_values = {frozenset(response_type.split(" ")) for response_type in
+                           provider_response_types}
+        common_response_types = client_values & provider_values
+        if not common_response_types:
+            raise OIDCFederationError(
+                    "Mismatch in registration request, no common response_types from client '{}' and provider response_types_supported '{}'.".format(
+                            client_response_types, provider_response_types))
+
+        return [" ".join(rt) for rt in common_response_types]
--- tests/oidc_fed/test_provider.py
+++ tests/oidc_fed/test_provider.py
@@ -1,12 +1,17 @@
 import json
+from collections import Counter
 
+import pytest
 from Crypto.PublicKey import RSA
 from jwkest.jwk import RSAKey, SYMKey
 from jwkest.jws import JWS
+from oic.extension.signed_http_req import SignedHttpRequest
 from oic.oauth2 import rndstr
 
+from oidc_fed import OIDCFederationError
 from oidc_fed.federation import Federation
-from oidc_fed.provider import OP
+from oidc_fed.messages import FederationRegistrationRequest, FederationRegistrationResponse
+from oidc_fed.provider import OP, RegistrationRequestVerification
 
 
 def rsa_key():
@@ -40,3 +45,131 @@ class TestOP(object):
         actual_metadata_parameters = JWS().verify_compact(provider_config["signed_metadata"],
                                                           keys=[op.intermediary_key]).keys()
         assert set(actual_metadata_parameters) == expected_metadata_parameters
+
+    def test_register_client(self):
+        federation_key = sym_key()
+        federation = Federation(federation_key)
+
+        op_root_key = rsa_key()
+        op_software_statement = federation.create_software_statement(
+                dict(root_key=json.dumps(op_root_key.serialize(private=False)),
+                     response_types_supported=["code"]))
+        op = OP(None, None, [op_software_statement], [federation_key], None)
+
+        rp_root_key = rsa_key()
+        rp_intermediary_key = rsa_key()
+        rp_signed_intermediary_key = JWS(json.dumps(rp_intermediary_key.serialize(private=False)),
+                                         alg=rp_root_key.alg).sign_compact(keys=[rp_root_key])
+        rp_software_statement = federation.create_software_statement(
+                dict(root_key=json.dumps(rp_root_key.serialize(private=False)),
+                     response_types=["code"]))
+        client_metadata = {
+            "signing_key": rp_signed_intermediary_key,
+            "signed_jwks_uri": "https://rp.example.com/signed_jwks",
+            "software_statements": [rp_software_statement],
+            "redirect_uris": ["https://rp.example.com"],
+            "response_types": ["id_token"]
+        }
+        req = FederationRegistrationRequest(**client_metadata)
+        signature = SignedHttpRequest(rp_intermediary_key).sign(rp_intermediary_key.alg,
+                                                                body=req.to_json())
+
+        client_metadata = op.register_client({"Authorization": "pop {}".format(signature)},
+                                      req.to_json())
+        registration_response = FederationRegistrationResponse().from_dict(client_metadata)
+        assert registration_response.verify()
+        assert "client_id" in registration_response
+        assert registration_response["provider_software_statement"] == op_software_statement
+        assert registration_response["response_types"] == ["code"]
+
+    def test_register_client_reject_request_without_authorization(self):
+        with pytest.raises(OIDCFederationError) as exc:
+            OP(None, None, [], None, None).register_client({}, None)
+
+        assert "Authorization" in str(exc.value)
+
+    def test_register_client_rejects_request_with_wrong_auth_scheme(self):
+        with pytest.raises(OIDCFederationError) as exc:
+            OP(None, None, [], None, None).register_client({"Authorization": "Basic foobar"},
+                                                           None)
+
+        assert "Authentication scheme" in str(exc.value)
+
+
+class TestRegistrationRequestVerification(object):
+    def test_verify(self):
+        provider_capabilities = {"id_token_signing_alg_values_supported": ["RS256", "RS512"],
+                                 "request_object_signing_alg_values_supported": ["RS256"]}
+        client_preferences = {"id_token_signed_response_alg": "RS512",
+                              "userinfo_signed_response_alg": "RS512"}
+
+        verifier = RegistrationRequestVerification()
+        matched_prefs = verifier.verify(provider_capabilities, {}, client_preferences, {})
+        assert matched_prefs == client_preferences
+
+    def test_verify_rejects_conflicting_request(self):
+        provider_capabilities = {"id_token_signing_alg_values_supported": ["RS256"]}
+        client_preferences = {"id_token_signed_response_alg": "RS512"}
+
+        with pytest.raises(OIDCFederationError):
+            RegistrationRequestVerification().verify(provider_capabilities, {}, client_preferences,
+                                                     {})
+
+    def test_provider_software_statement_overrides(self):
+        provider_capabilities = {
+            "id_token_signing_alg_values_supported": ["RS256"],
+        }
+        provider_software_statement = {"id_token_signing_alg_values_supported": ["RS512"]}
+
+        client_preferences = {"id_token_signed_response_alg": "RS512"}
+
+        matched_prefs = RegistrationRequestVerification().verify(provider_capabilities,
+                                                                 provider_software_statement,
+                                                                 client_preferences, {})
+        assert matched_prefs == client_preferences
+
+    def test_client_software_statement_overrides(self):
+        provider_capabilities = {
+            "id_token_signing_alg_values_supported": ["RS256", "RS512"],
+        }
+
+        client_preferences = {"id_token_signed_response_alg": "RS256"}
+        client_software_statement = {"id_token_signed_response_alg": "RS512"}
+
+        matched_prefs = RegistrationRequestVerification().verify(provider_capabilities, {},
+                                                                 client_preferences,
+                                                                 client_software_statement)
+        assert matched_prefs == client_software_statement
+
+    def test_provider_software_statement_is_default(self):
+        provider_capabilities = {
+            "id_token_signing_alg_values_supported": ["RS256"],
+        }
+        provider_software_statement = {"id_token_signing_alg_values_supported": ["RS512"]}
+
+        matched_prefs = RegistrationRequestVerification().verify(provider_capabilities,
+                                                                 provider_software_statement,
+                                                                 {}, {})
+        assert matched_prefs == {"id_token_signed_response_alg": "RS512"}
+
+    def test_client_preferences_as_arrays_are_matched(self):
+        client_preferences = {
+            "response_types": ["code", "token id_token"],
+            "grant_types": ["authorization_code", "implicit"],
+            "default_acr_values": ["u2f", "password"]
+        }
+        provider_capabilities = {
+            "response_types_supported": ["code", "code id_token token", "id_token token"],
+            "grant_types_supported": ["authorization_code"],
+            "acr_values_supported": ["uaf", "u2f"]
+        }
+
+        matched_prefs = RegistrationRequestVerification().verify(provider_capabilities, {},
+                                                                 client_preferences, {})
+
+        assert {frozenset(response_type.split(" ")) for response_type in
+                matched_prefs["response_types"]} == \
+               {frozenset(response_type.split(" ")) for response_type in
+                ["code", "token id_token"]}
+        assert matched_prefs["grant_types"] == ["authorization_code"]
+        assert matched_prefs["default_acr_values"] == ["u2f"]
