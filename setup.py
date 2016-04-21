a2f61ee982779d917bcfb315a7b0f5cbe248f853
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-04-21 19:49:51 +0200

    Cleanup setup.py.

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ setup(
     name='oidc-fed',
     version='0.0.1',
     description='Example implementation of model for OpenID Connect federations.',
-    author='DIRG',
-    author_email='dirg@its.umu.se',
+    author='Rebecka Gulliksson',
+    author_email='rebecka.gulliksson@its.umu.se',
     license='Apache 2.0',
     url='https://github.com/its-dirg/oidc-fed',
     packages=find_packages('src/', exclude=['services*']),
     package_dir={'': 'src'},
     install_requires=[
-        "oic",
+        'oic==0.8.3',
     ],
     zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ]
 )
